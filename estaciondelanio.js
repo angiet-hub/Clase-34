645674f4e5c0abde8be3eb739afe3b1f56a399e4
Angie Turne <angie.turne@hotmail.com>
2020-02-17 12:44:36 -0300

    clase 34

--- estaciondelanio.js
+++
@@ -1,7 +0,0 @@
-//Crear un programa que pregunte por el día, el mes, y el hemisferio (norte/sur) y devuelva la estación del año 
-//correspondiente
-
-const dia = Number(prompt("Ingrese el dia"));
-const mes = prompt("Ingrese el mes");
-const hemisferio = prompt("Ingrese el hemisferio");
-
